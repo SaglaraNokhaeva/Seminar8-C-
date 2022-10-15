a3c786ad29b66811ceb8b7321ca4adc65dd21796
Saglara <nohaevass@yandex.ru>
2022-10-15 19:23:36 +0300

    Задачи 54, 56, 58

--- Seminar8-domashka/Program.cs
+++ Seminar8-domashka/Program.cs
@@ -107,8 +107,7 @@ void Task56()
     Console.WriteLine("Суммированный массив: ");
     PrintArrTrans(arraySumm);
     int minSummRow = MinElement(arraySumm);
-    int m = MinElement(arraySumm);
- Console.WriteLine(m);
+    Console.WriteLine(minSummRow);
 
 
     int[,] FillArray(int rows, int columns, int min, int max)
@@ -127,10 +126,10 @@ void Task56()
     int[] SummArray(int[,] inputArr, int rows, int columns)
     {
         int[] summedArray = new int[rows];
-        for (int i = 0; i < columns; i++)
+        for (int i = 0; i < rows; i++)
         {
             summedArray[i] = 0;
-            for (int j = 0; j < rows; j++)
+            for (int j = 0; j < columns; j++)
             {
                 summedArray[i] = summedArray[i] + inputArr[i, j];
             }
@@ -159,77 +158,78 @@ void Task56()
         Console.WriteLine();
     };
 
-    int MinElement(int[] inputArray);
+    int MinElement(int[] inputArray)
     {
         int minElem = inputArray[0];
+        int k = 0;
         for (int i = 0; i < inputArray.Length - 1; i++)
         {
-            int k = i;
             if (inputArray[i] < minElem)
             {
+                k = i;
                 minElem = inputArray[i];
             }
         };
         return k;
     };
 };
-    void Task52()
+void Task52()
+{
+    //Задача 52. Задайте двумерный массив из целых чисел. 
+    //Найдите среднее арифметическое элементов в каждом столбце.
+
+    // Например, задан массив:
+    // 1 4 7 2
+    // 5 9 2 3
+    // 8 4 2 4
+    // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+
+    Console.Clear();
+    Console.Write("Введите количество строк: ");
+    int rowCount = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите количество столбцов: ");
+    int columnCount = Convert.ToInt32(Console.ReadLine());
+
+    int[,] array = FillArray(rowCount, columnCount, 1, 10);
+    Console.WriteLine("Массив: ");
+    PrintArr(array);
+
+    for (int j = 0; j < columnCount; j++)
     {
-        //Задача 52. Задайте двумерный массив из целых чисел. 
-        //Найдите среднее арифметическое элементов в каждом столбце.
-
-        // Например, задан массив:
-        // 1 4 7 2
-        // 5 9 2 3
-        // 8 4 2 4
-        // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
-
-
-
-        Console.Clear();
-        Console.Write("Введите количество строк: ");
-        int rowCount = Convert.ToInt32(Console.ReadLine());
-
-        Console.Write("Введите количество столбцов: ");
-        int columnCount = Convert.ToInt32(Console.ReadLine());
-
-        int[,] array = FillArray(rowCount, columnCount, 1, 10);
-        Console.WriteLine("Массив: ");
-        PrintArr(array);
-
-        for (int j = 0; j < columnCount; j++)
+        double summa = 0;
+        for (int i = 0; i < rowCount; i++)
         {
-            double summa = 0;
-            for (int i = 0; i < rowCount; i++)
-            {
-                summa = summa + array[i, j];
-            }
-            double averageArif = summa / rowCount;
-            Console.Write(Math.Round(averageArif, 2) + "; ");
+            summa = summa + array[i, j];
         }
+        double averageArif = summa / rowCount;
+        Console.Write(Math.Round(averageArif, 2) + "; ");
+    }
 
-        int[,] FillArray(int rows, int columns, int min, int max)
+    int[,] FillArray(int rows, int columns, int min, int max)
+    {
+        int[,] filledArray = new int[rows, columns];
+        for (int i = 0; i < rows; i++)
         {
-            int[,] filledArray = new int[rows, columns];
-            for (int i = 0; i < rows; i++)
+            for (int j = 0; j < columns; j++)
             {
-                for (int j = 0; j < columns; j++)
-                {
-                    filledArray[i, j] = new Random().Next(min, max + 1);
-                }
+                filledArray[i, j] = new Random().Next(min, max + 1);
             }
-            return filledArray;
-        };
+        }
+        return filledArray;
+    };
 
-        void PrintArr(int[,] inputArray)
+    void PrintArr(int[,] inputArray)
+    {
+        for (int i = 0; i < inputArray.GetLength(0); i++)
         {
-            for (int i = 0; i < inputArray.GetLength(0); i++)
+            for (int j = 0; j < inputArray.GetLength(1); j++)
             {
-                for (int j = 0; j < inputArray.GetLength(1); j++)
-                {
-                    Console.Write(" " + inputArray[i, j]);
-                }
-                Console.WriteLine();
+                Console.Write(" " + inputArray[i, j]);
             }
+            Console.WriteLine();
         }
     }
+}
