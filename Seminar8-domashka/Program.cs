bbe07d7da9e42e25c1597dbfdd5069b467311ddc
Saglara <nohaevass@yandex.ru>
2022-10-15 20:19:05 +0300

    Задачи 54, 56, 58

--- Seminar8-domashka/Program.cs
+++ Seminar8-domashka/Program.cs
@@ -1,9 +1,9 @@
 ﻿Console.Clear();
-Console.WriteLine("Введите номер задачи (54, 56 или 52) для проверки:");
+Console.WriteLine("Введите номер задачи (54, 56 или 58) для проверки:");
 int num = int.Parse(Console.ReadLine());
 if (num == 54) Task54();
 else if (num == 56) Task56();
-else if (num == 52) Task52();
+else if (num == 58) Task58();
 else Console.WriteLine("Дополнительных задач не решала.");
 
 void Task54()
@@ -107,7 +107,7 @@ void Task56()
     Console.WriteLine("Суммированный массив: ");
     PrintArrTrans(arraySumm);
     int minSummRow = MinElement(arraySumm);
-    Console.WriteLine(minSummRow);
+    Console.WriteLine("Номер строки с наименьшей суммой элементов: "+ minSummRow);
 
 
     int[,] FillArray(int rows, int columns, int min, int max)
@@ -173,41 +173,38 @@ void Task56()
         return k;
     };
 };
-void Task52()
+void Task58()
 {
-    //Задача 52. Задайте двумерный массив из целых чисел. 
-    //Найдите среднее арифметическое элементов в каждом столбце.
-
-    // Например, задан массив:
-    // 1 4 7 2
-    // 5 9 2 3
-    // 8 4 2 4
-    // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
-
+    //Задача 58: Задайте две матрицы. Напишите программу, которая будет 
+    //находить произведение двух матриц.
+    // Например, даны 2 матрицы:
+    // 2 4 | 3 4
+    // 3 2 | 3 3
+    // Результирующая матрица будет:
+    // 18 20
+    // 15 18
 
 
     Console.Clear();
-    Console.Write("Введите количество строк: ");
-    int rowCount = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите количество строк первого массива: ");
+    int rowCountFirst = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите количество столбцов первого массива: ");
+    int columnCountFirst = Convert.ToInt32(Console.ReadLine());
 
-    Console.Write("Введите количество столбцов: ");
-    int columnCount = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите количество строк второго массива: ");
+    int rowCountSecond = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите количество столбцов второго массива: ");
+    int columnCountSecond = Convert.ToInt32(Console.ReadLine());
 
-    int[,] array = FillArray(rowCount, columnCount, 1, 10);
-    Console.WriteLine("Массив: ");
-    PrintArr(array);
+    int[,] arrayFirst = FillArray(rowCountFirst, columnCountFirst, 1, 10);
+    Console.WriteLine("Первый массив: ");
+    PrintArr(arrayFirst);
+    int[,] arraySecond = FillArray(rowCountSecond, columnCountSecond, 1, 10);
+    Console.WriteLine("Второй массив: ");
+    PrintArr(arraySecond);
 
-    for (int j = 0; j < columnCount; j++)
-    {
-        double summa = 0;
-        for (int i = 0; i < rowCount; i++)
-        {
-            summa = summa + array[i, j];
-        }
-        double averageArif = summa / rowCount;
-        Console.Write(Math.Round(averageArif, 2) + "; ");
-    }
-
+    MyltiplayArray(arrayFirst, arraySecond);
+   
     int[,] FillArray(int rows, int columns, int min, int max)
     {
         int[,] filledArray = new int[rows, columns];
@@ -231,5 +228,24 @@ void Task52()
             }
             Console.WriteLine();
         }
+    };
+
+};
+
+void MyltiplayArray(int[,] inputArrFirst, int[,] inputArrSecond)
+{
+    if ((inputArrFirst.GetLength(0) == inputArrSecond.GetLength(0)) && (inputArrFirst.GetLength(1) == inputArrSecond.GetLength(1)))
+    {
+        Console.WriteLine("Произведение массивов");
+        int[,] MyltiArr = new int[inputArrFirst.GetLength(0), inputArrFirst.GetLength(1)];
+        for (int i = 0; i < inputArrFirst.GetLength(0); i++)
+        {
+            for (int j = 0; j < inputArrFirst.GetLength(1); j++)
+            {
+                Console.Write(" " + inputArrFirst[i, j] * inputArrSecond[i, j]);
+            }
+            Console.WriteLine();
+        }
     }
+    else Console.WriteLine("Невозможно выполнить произведение матриц.");
 }
